79972a96846ac584912fcdd63c0555323378b657
saiyan-derekcodez <derekdavid866@gmail.com>
2023-05-25 11:35:48 +0100

    added cors feature

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import db from './db.json' assert {type: "json"};
 import bodyParser from "body-parser";
+import cors from 'cors';
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.status(200).json(db);
